45a48f31cfe0a09aef0f12ebe44c29c395a12362
YunJae00 <0109548@naver.com>
2024-09-02 17:12:41 +0900

    5강. 실시간 설문조사 웹서비스 구현 (4) - 설문조사 주제를 보여주는 메인페이지 구현

--- site_2/polls/views.py
+++ site_2/polls/views.py
@@ -1,6 +1,28 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Question, Choice
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # Question.objects.all()
+    latest_question_list =  Question.objects.order_by('-pub_date')[:5]
+
+    context = {'latest_question_list': latest_question_list}
+
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    return Question.objects.get(id=question_id)
+
+
+def vote(request, question_id):
+    question = Question.objects.get(id=question_id)
+
+
+def results(request, question_id):
+    question = Question.objects.get(id=question_id)
+    choices = question.choice_set.all()
+    return choices.order_by('choice_text')
+
+
