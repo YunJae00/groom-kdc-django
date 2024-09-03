fa6bab708f4c3f90aad3e1cca394cf02e95a28f2
YunJae00 <0109548@naver.com>
2024-09-03 13:49:28 +0900

    8강. 실시간 설문조사 웹서비스 구현 (7) - HTTP POST request 처리 & 설문조사 투표 결과 노출

--- site_2/polls/views.py
+++ site_2/polls/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, Http404
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Question, Choice
 
 # Create your views here.
@@ -29,12 +29,26 @@ def detail(request, question_id):
 
 
 def vote(request, question_id):
-    question = Question.objects.get(id=question_id)
+
+    question = get_object_or_404(Question, id=question_id)
+
+    # print(question_id)
+
+    try:
+        selected_choice = question.choice_set.get(id=request.POST['choice_select'])
+    except:
+        context = {'question': question, 'error_message': 'Please select a choice'}
+        return render(request, 'polls/detail.html', context)
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # url 에서 별명 name 으로 불러옴
+        return redirect('polls:results', question_id=question.id)
+
 
 
 def results(request, question_id):
-    question = Question.objects.get(id=question_id)
-    choices = question.choice_set.all()
-    return choices.order_by('choice_text')
+    question = get_object_or_404(Question, id=question_id)
+    return render(request, 'polls/result.html', {'question': question})
 
 
