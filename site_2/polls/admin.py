451e7affc4dfe9be550261fa6556ca4ac493e6cc
YunJae00 <0109548@naver.com>
2024-09-04 02:53:43 +0900

    9강. 실시간 설문조사 웹서비스 구현 (8) - 관리자 페이지 Customization 적용

--- site_2/polls/admin.py
+++ site_2/polls/admin.py
@@ -2,6 +2,43 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
+
+# question 만들 때 choice 도 같이 보이게
+# stacked 는 위아래로 tabular 는 table 형식으로 (가로로)
+# class ChoiceInline(admin.StackedInline):
+#     model = Choice
+#     extra = 2 # default 로 보여줄 Choice 입력 slot 의 수
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2 # default 로 보여줄 Choice 입력 slot 의 수
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ['question_text', 'pub_date']
+    fieldsets = [
+        # group (Question title) 에 들어갈 fields 들 (list)
+        ("Question title", {'fields': ['question_text']}),
+        # collapse 는 굳이 안보여줘도 되는거는 안보이게 (classes는 스타일 느낌임 디자인)
+        ("Date information", {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+
+    # choice 추가
+    inlines = [ChoiceInline]
+
+    # question 리스트 보여줄 때 어떤거 보여주는지 표시
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+    # filter 를 만들어주고 어떤 기준으로 만들어주냐 (어떤걸 기준으로 잡냐를 list 안에 넣어줌)
+    list_filter = ['pub_date']
+
+    # search 가능하게
+    search_fields = ['question_text']
+
+
+
+
+
 # Register your models here.
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
--- site_2/polls/models.py
+++ site_2/polls/models.py
@@ -16,6 +16,13 @@ class Question(models.Model):
         now = timezone.now()
         return now >= self.pub_date >= now - datetime.timedelta(days=1)
 
+    # 이거 admin 페이지에서 볼 수 있음 true false 가 아니라 o x 로 나오게 변경
+    was_published_recently.boolean = True
+    # 이 칼럼으로 정렬할 때 pub_date 를 기준으로 정렬해라 라고 명시해줌
+    was_published_recently.admin_order_field = 'pub_date'
+    # 의미를 조금 짧게 설명해줌
+    was_published_recently.short_description = 'Published recently?'
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE) # 알아서 question의 pk를 땡겨와서 연결해줌
