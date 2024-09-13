cf1b5a0b9c6a15bbea7cb5b906e089a883438abc
YunJae00 <0109548@naver.com>
2024-09-14 03:41:39 +0900

    04강. 예측모델 기반 Face detection 웹서비스 구 (3) - 유저 업로드 파일 저장을 위한 FileSystemStorage 활용

--- cv_project/opencv_webapp/views.py
+++ cv_project/opencv_webapp/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 from .forms import SimpleUploadForm
 
+# 파일 저장하기 위한 도구 db 있을 땐 이거 안씀
+from django.core.files.storage import FileSystemStorage
+
 
 # Create your views here.
 def first_view(request):
@@ -9,6 +12,35 @@ def first_view(request):
 
 
 def simple_upload(request):
-    form = SimpleUploadForm()
-    context = {'form': form}
-    return render(request, 'opencv_webapp/simple_upload.html', context)
+
+    if request.method == 'POST':
+        # print(request.POST) : <QueryDict: {'csrfmiddlewaretoken': [‘~~~’], 'title': ['upload_1']}>
+        # print(request.FILES) : <MultiValueDict: {'image': [<InMemoryUploadedFile: ses.jpg (image/jpeg)>]}>
+        # 메모리 상에 잠깐 올라가 있는 이미지 파일을 사용해야함
+        # 비어있는 Form에 사용자가 업로드한 데이터를 넣고 검증합니다.
+
+        #request.POST 'title'
+        #request.FILES 'image'
+        form = SimpleUploadForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            myfile = request.FILES['image']
+
+            fs = FileSystemStorage()
+            # params 경로명을 포함할 파일 저장 시 활용할 이름, 파일 객체 자체
+            filename = fs.save(myfile.name, myfile) # 저장이 끝난 물리적인 파일 이름
+
+            # myfile.name : 'ses.jpg' (사용자가 업로드한 파일 원본의 이름)
+            # filename : 'ses_UPArih4.jpg' (서버에 업로드가 끝난 파일의 이름, 중복될 시 자동으로 변경됨)
+            # 이거의 type 을 찍어보면 두개는 다른거임 string 이랑 파일 자체로 다른거임
+
+            uploaded_file_url =  fs.url(filename) # 저장이 끝난 물리적인 파일로 접근 가능한 URL
+
+            context = {'form':form, 'uploaded_file_url': uploaded_file_url}
+
+            return render(request, 'opencv_webapp/simple_upload.html', context)
+
+    else:
+        form = SimpleUploadForm()
+        context = {'form': form}
+        return render(request, 'opencv_webapp/simple_upload.html', context)
