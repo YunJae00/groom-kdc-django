f7a5d29c94b30d42be27660e11e38c94a2e96a26
YunJae00 <0109548@naver.com>
2024-09-18 02:52:06 +0900

    08강. 예측모델 기반 Face detection 웹서비스 구 (7) - OpenCV 기반 자동 이미지 리사이징 적용

--- cv_project/opencv_webapp/admin.py
+++ cv_project/opencv_webapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import ImageUploadModel
+
+
+class ImageUploadAdmin(admin.ModelAdmin):
+    list_display = ('description', 'document', ) # list_display 변수명은 고정
+
+
+admin.site.register(ImageUploadModel, ImageUploadAdmin)
